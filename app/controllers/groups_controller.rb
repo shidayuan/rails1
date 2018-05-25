ab4f962c660b6fc680f2ee92d274752011605dd5
shichengyuan <15036062755@163.com>
2018-05-25 16:21:49 +0800

    sldj

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -14,8 +14,11 @@ class GroupsController < ApplicationController
 
   def create
     @group = Group.new(group_params)
-    @group.save
+    if @group.save
     redirect_to groups_path flash[:notice] = "created success"
+  else
+    render :new
+  end
 
   end
 
@@ -25,8 +28,12 @@ class GroupsController < ApplicationController
 
   def update
     @group = Group.find(params[:id])
-    @group.update(group_params)
+    if @group.update(group_params)
     redirect_to groups_path flash[:notice] = "Updated success"
+    else
+      render :edit
+    end 
+
   end
 
   def destroy
